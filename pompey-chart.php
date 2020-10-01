772944facc0918db7720d88ad0ca28e138e83d22
Herb Miller <herb@bobbingwide.com>
2020-10-01 18:12:08 +0100

    Reworked to parameterised shortcode, with height= attribute

--- pompey-chart.php
+++ pompey-chart.php
@@ -24,7 +24,11 @@ function pompey_chart_init() {
 
 
 function pompey_chart_enqueue_scripts() {
-	wp_enqueue_script('leaflet-script', 'https://unpkg.com/leaflet@1.0.3/dist/leaflet.js' );
+	wp_enqueue_style( 'chart-style', 'https://www.andrew-leonard.co.uk/Chart/Tooltip.css' );
+	wp_enqueue_style( 'chart-extra-style', 'https://www.andrew-leonard.co.uk/Chart/Extra.css' );
+	wp_enqueue_style( 'chartist-style', 'https://cdnjs.cloudflare.com/ajax/libs/chartist/0.10.1/chartist.min.css' );
+
+	//wp_enqueue_script('leaflet-script', 'https://unpkg.com/leaflet@1.0.3/dist/leaflet.js' );
 	wp_enqueue_script('chartist-script', 'https://cdnjs.cloudflare.com/ajax/libs/chartist/0.10.1/chartist.min.js' );
 	wp_enqueue_script('chartist-tooltip-script', 'https://unpkg.com/chartist-plugin-tooltips@0.0.17/dist/chartist-plugin-tooltip.js' );
 	wp_enqueue_script('chartist-legend-script', 'https://cdnjs.cloudflare.com/ajax/libs/chartist-plugin-legend/0.6.1/chartist-plugin-legend.min.js' );
@@ -46,49 +50,116 @@ function pompey_chart_enqueue_scripts() {
  */
 function pompey_chart_shortcode( $atts, $content, $tag ) {
 	$html = "Pompey Chart";
-	//$html = pompey_chart();
+	$lines = pompey_chart_data( $atts, $content );
+	$height = isset( $atts['height']) ? $atts['height'] : "450px";
+	$html .= pompey_chart( $lines, $height );
 	return $html;
 }
 
+function pompey_chart_data( $atts, $content ) {
+	if ( $content ) {
+		$lines = $content;
+	} else {
+		$lines =file( 'https://www.andrew-leonard.co.uk/Chart/meetup-stats.csv', FILE_IGNORE_NEW_LINES );
+	}
+	return $lines;
+}
 
-function pompey_chart()
-{
-	$lines = file('https://www.andrew-leonard.co.uk/Chart/meetup-stats.csv', FILE_IGNORE_NEW_LINES);
-	$Date="";
-	$null="";
-	$tick="'";
-	$comma=",";
+
+function pompey_chart( $lines, $height ) {
+
+	//$html =count( $lines );
+	$html = "<div class=\"ct-chart\"></div>";
+	$script = pompey_chart_process( $lines, $height );
+	$html .= pompey_chart_inline_script(  $script );
+	return $html;
+}
+
+function pompey_chart_inline_script( $script  ) {
+	$html = '<script type="text/javascript">';
+	$html .= $script;
+	$html .= '</script>';
+	return $html;
+}
+
+function pompey_chart_process( $lines, $height ) {
+
+	$Date   ="";
+	$null   ="";
+	$tick   ="'";
+	$comma  =",";
 	$series1="";
 	$series2="";
 	$series3="";
-	$bit1="{meta: ";
-	$bit2=",value: ";
-	$bit3="}";
+	$bit1   ="{meta: ";
+	$bit2   =",value: ";
+	$bit3   ="}";
 	$newline="<br>\n";
-	foreach ($lines as $line_num => $line)
-	{
-		if  (substr($line,0,4)<>"Date")
-		{
-			if  (substr($line,6,4)<>",,,,")
-			{
-				$line=str_replace("'","",$line);
-				$line=str_replace('"',"",$line);
-				$pieces = explode(",", $line);
-				if ($Date=="") {$Date= $tick.$pieces[0].$tick;}
-				else {$Date.= $comma.$tick.$pieces[0].$tick;}
-				if ($series1=="") {$series1= $bit1.$tick.$pieces[4].$tick.$bit2.$pieces[1].$bit3;}
-				else {$series1.= $comma.$bit1.$tick.$pieces[4].$tick.$bit2.$pieces[1].$bit3;;}
-				if ($series2=="") {$series2= $bit1.$tick.$tick.$bit2.$pieces[2].$bit3;}
-				else {$series2.= $comma.$bit1.$tick.$tick.$bit2.$pieces[2].$bit3;;}
-				if ($series3=="") {$series3= $bit1.$tick.$tick.$bit2.$pieces[3].$bit3;}
-				else {$series3.= $comma.$bit1.$tick.$tick.$bit2.$pieces[3].$bit3;;}
+	foreach ( $lines as $line_num=>$line ) {
+		//$line = trim( ',', $line );
+		if ( substr( $line, 0, 4 ) <> "Date" ) {
+			if ( substr( $line, 6, 4 ) <> ",,,," ) {
+				$line  =str_replace( "'", "", $line );
+				$line  =str_replace( '"', "", $line );
+				$pieces=explode( ",", $line );
+				if ( $Date == "" ) {
+					$Date=$tick . $pieces[0] . $tick;
+				} else {
+					$Date.=$comma . $tick . $pieces[0] . $tick;
+				}
+				if ( $series1 == "" ) {
+					$series1=$bit1 . $tick . $pieces[4] . $tick . $bit2 . $pieces[1] . $bit3;
+				} else {
+					$series1.=$comma . $bit1 . $tick . $pieces[4] . $tick . $bit2 . $pieces[1] . $bit3;;
+				}
+				if ( $series2 == "" ) {
+					$series2=$bit1 . $tick . $tick . $bit2 . $pieces[2] . $bit3;
+				} else {
+					$series2.=$comma . $bit1 . $tick . $tick . $bit2 . $pieces[2] . $bit3;;
+				}
+				if ( $series3 == "" ) {
+					$series3=$bit1 . $tick . $tick . $bit2 . $pieces[3] . $bit3;
+				} else {
+					$series3.=$comma . $bit1 . $tick . $tick . $bit2 . $pieces[3] . $bit3;;
+				}
 			}
 		}
 	}
-	$Data="var chart=new Chartist.Line('.ct-chart',{labels:[".$Date."],series:[[".$series1."],[".$series2."],[".$series3."]]},{fullWidth:true,width:'100%',height:'700px',chartPadding:{right:50,left:50},plugins:[Chartist.plugins.tooltip(),Chartist.plugins.legend({legendNames:['Attendees','Joined','Members'],})]});";
+	$data = pompey_chart_javascript( $Date, $series1, $series2, $series3, $height );
+	return $data;
+}
+
+function pompey_chart_javascript( $Date, $series1, $series2, $series3, $height = '500px') {
+
+	$Data = "var chart=new Chartist.Line('.ct-chart',{labels:[";
+	$Data .= $Date;
+	$Data .=  '],';
+	$Data .= 'series:[[';
+	$Data .= $series1;
+	$Data .= '],[';
+	$Data .= $series2;
+	$Data .= '],[';
+	$Data .= $series3;
+	$Data .= ']]},';
+	$Data .= "{fullWidth:true,width:'100%',height:'";
+	$Data .= $height;
+	$Data .= "',chartPadding:{right:50,left:50},";
+	$Data .= "plugins:[Chartist.plugins.tooltip(),Chartist.plugins.legend({legendNames:['Attendees','Joined','Members'],})]});";
+	return $Data;
+}
+	/*
 	echo "<h1 style=\"text-align:center;\">Portsmouth MeetUp Statistics</h1>";
 	echo "<div class=\"ct-chart\"></div>";
 	echo "<script>";
 	echo $Data;
 	echo "</script>";
-}
+	*/
+
+/*
+ * $Data="var chart=new Chartist.Line('.ct-chart',{labels:[".$Date."],
+ * series:[[".$series1."],[".$series2."],[".$series3."]]}
+ * ,{fullWidth:true,width:'100%',height:'700px',chartPadding:{right:50,left:50}
+ * ,plugins:[Chartist.plugins.tooltip(),Chartist.plugins.legend({legendNames:['Attendees','Joined','Members'],})]});";
+
+ */
+
