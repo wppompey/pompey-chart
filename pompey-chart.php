f722ea65d013e9d4faf7702e4ecac7920a538461
Herb Miller <herb@bobbingwide.com>
2020-12-28 22:05:19 +0000

    Issue #4 - start creating generic [chart] shortcode

--- pompey-chart.php
+++ pompey-chart.php
@@ -3,7 +3,7 @@
  * Plugin Name:     Pompey Chart
  * Plugin URI: 		https://github.com/wppompey/pompey-chart.git
  * Description:     Displays a chart of WordPress Portsmouth Meetup attendees over time
- * Version:         0.0.0
+ * Version:         0.1.0
  * Author:          andrewleonard, bobbingwide
  * Author URI: 		https://wp-pompey.org.uk
  * License:         GPL-2.0-or-later
@@ -19,41 +19,290 @@ add_action( 'init', 'pompey_chart_init' );
 
 function pompey_chart_init() {
 	add_shortcode( 'pompey_chart', 'pompey_chart_shortcode');
-	add_action( 'wp_enqueue_scripts', 'pompey_chart_enqueue_local_scripts' );
+	add_shortcode( 'chart', 'pompey_chart_chart_shortcode');
+	add_action( 'wp_enqueue_scripts', 'pompey_chart_enqueue_scripts' );
 }
 
 /**
- * Chartist and its plugins
+ * Chartist and its plugins:
  * - https://github.com/gionkunz/chartist-js/
  * - https://github.com/tmmdata/chartist-plugin-tooltip
  * - https://github.com/CodeYellowBV/chartist-plugin-legend
+ *
+ * @TODO Tooltips don't work when enqueued locally. Probably a versioning problem.
+ * Need to find out what source Andrew used that pointed him to cloudflare and unpkg.
  */
-function pompey_chart_enqueue_local_scripts() {
-	$path = plugin_dir_url( __FILE__ );
 
+function pompey_chart_enqueue_local() {
+	pompey_chart_enqueue_local_styles();
+	pompey_chart_enqueue_local_scripts();
+}
+
+function pompey_chart_enqueue_local_styles() {
+	$path=plugin_dir_url( __FILE__ );
 	wp_enqueue_style( 'chartist-style', $path . '/dist/chartist.min.css' );
 	wp_enqueue_style( 'chart-style', $path . '/Chart/Tooltip.css' );
 	wp_enqueue_style( 'chart-extra-style', $path . '/Chart/Extra.css' );
+}
 
+function pompey_chart_enqueue_local_scripts() {
+	$path = plugin_dir_url( __FILE__ );
 	wp_enqueue_script('chartist-script', $path . '/dist/chartist.min.js' );
 	wp_enqueue_script('chartist-tooltip-script', $path . '/dist/chartist-plugin-tooltip.js' );
 	wp_enqueue_script('chartist-legend-script', $path . '/dist/chartist-plugin-legend.js' );
 }
 
-
-
-
 function pompey_chart_enqueue_scripts() {
+	pompey_chart_enqueue_local_styles();
+
+	/*
+	$path = plugin_dir_url( __FILE__ );
+
 	wp_enqueue_style( 'chartist-style', 'https://cdnjs.cloudflare.com/ajax/libs/chartist/0.10.1/chartist.min.css' );
 	// __DIR__ /
 	wp_enqueue_style( 'chart-style', plugin_dir_url( __FILE__ ) . '/Chart/Tooltip.css' );
 	wp_enqueue_style( 'chart-extra-style', 'https://www.andrew-leonard.co.uk/Chart/Extra.css' );
-
+	*/
 
 	//wp_enqueue_script('leaflet-script', 'https://unpkg.com/leaflet@1.0.3/dist/leaflet.js' );
 	wp_enqueue_script('chartist-script', 'https://cdnjs.cloudflare.com/ajax/libs/chartist/0.10.1/chartist.min.js' );
 	wp_enqueue_script('chartist-tooltip-script', 'https://unpkg.com/chartist-plugin-tooltips@0.0.17/dist/chartist-plugin-tooltip.js' );
 	wp_enqueue_script('chartist-legend-script', 'https://cdnjs.cloudflare.com/ajax/libs/chartist-plugin-legend/0.6.1/chartist-plugin-legend.min.js' );
+}
+
+/**
+ * Implements the generic [chart] shortcode.
+ *
+ * @param array $atts Shortcode attributes.
+ * @param string $content The chart's raw data. CSV format
+ * @param string $tag The shortcode.
+ */
+function pompey_chart_chart_shortcode( $atts, $content, $tag ) {
+
+	bw_trace2();
+	$atts = pompey_chart_default_atts( $atts );
+
+
+	if ( $content ) {
+		$content=trim( $content );
+		$content=str_replace( '<br />', '', $content );
+		$lines  =explode( "\n", $content );
+	}
+	$legend = $lines[0];
+
+	$legend=array_shift( $lines );
+
+	//GetSeries( $lines );
+	$series=pompey_chart_transpose( $lines );
+	bw_trace2( $series, "series" );
+	//print_r( $series );
+	$id  =pompey_chart_id();
+	$html= "<div class=\"ct-chart\" id=\"$id\"></div>";
+	// Data: Labels and Series
+	$script = pompey_chart_data_label_series( $series, $atts );
+	$jsonlegend = pompey_chart_json_legend( $legend );
+	$script .= pompey_chart_options( $atts, $jsonlegend );
+	$script .= pompey_chart_type();
+	$script .= "'#$id',data,options );";
+	$html .=  pompey_chart_inline_script( $script );
+
+	// Parameters
+	// Plugins
+
+	//$script = pompey_chart_process( $id, $lines, $height );
+	return $html;
+}
+
+function pompey_chart_default_atts( $atts ) {
+	$atts['title']=isset( $atts['title'] ) ? $atts['title'] : 'Chart';
+	$atts['height'] = isset( $atts['height'] ) ? $atts['height'] : '450px';
+	return $atts;
+
+}
+
+/**
+ * Returns the data parameter for Chartist.
+ *
+ * If we need tooltips for a series then we have to use arrays of
+ * [ { meta: 'tooltip 1', value: value1 }, [meta: 'tooltip 2', value: value2 }, etc ]
+ * to replace the simple arrays:
+ * [ value1, value2 ]
+ *
+ *
+ * @param $series
+ * @param $atts
+ *
+ * @return false|string
+ */
+
+function pompey_chart_data_label_series(  $series, $atts ) {
+	// Assume tooltips are the last?
+	$tooltips = array_pop( $series );
+	$data = new stdClass();
+	$data->labels = array_shift( $series );
+	$data->series = [];
+	foreach (  $series as $index => $seriesn ) {
+		$data->series[] = pompey_chart_data_seriesn( $seriesn, $tooltips );
+	}
+	$json = json_encode( $data );
+
+	$script = "var data = $json;\n";
+	//echo $script;
+	return $script;
+
+}
+
+function pompey_chart_data_seriesn( $seriesn, $tooltips ) {
+	$metavalues=[];
+	foreach ( $seriesn as $key=>$value ) {
+		$metavalue=new StdClass();
+		$metavalue->meta =$tooltips[ $key ];
+		$metavalue->value=$value;
+		$metavalues[]    =$metavalue;
+	}
+	return $metavalues;
+}
+
+/**
+ * Returns Chartist options parameter.
+ *
+ * ```
+$Data .= "{fullWidth:true,width:'100%',height:'";
+$Data .= $height
+$Data .= "',chartPadding:{right:0,left:0},";
+$Data .= "plugins:[Chartist.plugins.tooltip(),Chartist.plugins.legend({legendNames:['Attendees','Joined','Members'],})]});";
+ * ```
+ * @param array $atts array of attributes
+ * @param $legend string Heading line - to use for Legends
+ * @return string
+ */
+
+function pompey_chart_options( $atts, $jsonlegend ) {
+	$options                     =new StdClass();
+	$options->fullWidth          =true;
+	$options->width              ='100%';
+	$options->height             =$atts['height'];
+	$options->chartPadding = new StdClass();
+	$options->chartPadding->right = 80;
+	$options->chartPadding->left = 40;
+	$options->plugins            = 'repl_plugins';
+	$json                        =json_encode( $options, JSON_UNESCAPED_SLASHES );
+	$plugins       ='Chartist.plugins.tooltip(),';
+	$plugins       .= 'Chartist.plugins.legend(' . $jsonlegend . ')';
+	$json = str_replace( '"repl_plugins"', "[$plugins]", $json);
+	$script                      ="var options = $json;\n";
+	//echo $script;
+
+	return $script;
+}
+
+/**
+ * Returns the legend for the Chartist legend plugin.
+ * JSON returns:
+ * {"legendNames":"Date,A,B,Tooltip"}
+ * {legendNames:['Attendees','Joined','Members'],
+ * but we want
+ *
+ * @param $legend
+ *
+ * @return false|string
+ */
+
+function pompey_chart_json_legend( $legend ) {
+	$legends = explode( ',', $legend);
+	array_shift( $legends);
+	$legendstring = "{legendNames:['";
+	$legendstring .= implode( "','", $legends);
+	$legendstring .= "'],}";
+	return $legendstring;
+}
+
+
+function pompey_chart_series( $series ) {
+	/*
+	foreach $series as $series
+	$Data .= 'series:[[';
+	$Data .= $series1;
+	$Data .= '],[';
+	$Data .= $series2;
+	$Data .= '],[';
+	$Data .= $series3;
+	$Data .= ']]},';
+	*/
+	$html ='';
+	return $html;
+}
+
+/**
+ * Returns the Chartist invocation.
+ *
+ * @param string $type
+ *
+ * @return string
+ */
+function pompey_chart_type( $type="Line") {
+		return "var chart=new Chartist.$type( ";
+	}
+	/*
+	function pompey_chart_javascript( $id, $Date, $series1, $series2, $series3, $height = '500px') {
+			bw_trace2();
+			$Data = "var chart=new Chartist.Line( '#$id'
+			,{labels:[";
+			$Data .= $Date;
+			$Data .=  '],';
+			$Data .= 'series:[[';
+			$Data .= $series1;
+			$Data .= '],[';
+			$Data .= $series2;
+			$Data .= '],[';
+			$Data .= $series3;
+			$Data .= ']]},';
+			$Data .= "{fullWidth:true,width:'100%',height:'";
+			$Data .= $height;
+			$Data .= "',chartPadding:{right:0,left:0},";
+			$Data .= "plugins:[Chartist.plugins.tooltip(),Chartist.plugins.legend({legendNames:['Attendees','Joined','Members'],})]});";
+			return $Data;
+		}
+	*/
+/*
+<p>Pompey Chart</p>
+<div class="ct-chart" id="chart1"></div>
+<p><script type="text/javascript">
+var chart=new Chartist.Line(
+'#chart1'
+,{labels:['Jul-20','Aug-20','Sep-20'],
+series:[
+[{meta: 'How to build your WordPress website - Part Two',value: 17},{meta: 'Plugin Kollektiv',value: 15},{meta: 'Whats new in WordPress 5.5. and how to use it',value: 39}],
+[{meta: '',value: 8},{meta: '',value: 7},{meta: '',value: 14}],
+[{meta: '',value: 187},{meta: '',value: 194},{meta: '',value: 208}]
+]}
+,{fullWidth:true,width:'100%',height:'400px',chartPadding:{right:0,left:0},
+plugins:[Chartist.plugins.tooltip(),Chartist.plugins.legend({legendNames:['Attendees','Joined','Members'],})]});
+</script>
+*/
+
+
+
+
+/**
+ * Transposes the input CSV into the series array.
+ *
+ * $series[0] will be the labels for the x-axis - along the bottom
+ * We assume that there's a value for each row and column.
+ *
+ * @param $lines
+ * @return array
+ */
+
+function pompey_chart_transpose( $lines ) {
+	$series = [];
+	foreach ( $lines as $line ) {
+		$values = explode( ',', $line);
+		foreach ( $values as $key => $value ) {
+			$series[ $key ][] = $value;
+		}
+	}
+	return $series;
 }
 
 
@@ -71,16 +320,9 @@ function pompey_chart_enqueue_scripts() {
  *
  */
 function pompey_chart_shortcode( $atts, $content, $tag ) {
-	//pompey_chart_enqueue_local_scripts();
-
-	// To support multiple charts in the post content
-	// we need to pass a unique ID
-
-
 	$html = "Pompey Chart";
 	$lines = pompey_chart_data( $atts, $content );
 	$height = isset( $atts['height']) ? $atts['height'] : "450px";
-
 	$html .= pompey_chart( $lines, $height );
 	return $html;
 }
@@ -117,8 +359,6 @@ function pompey_chart_data( $atts, $content ) {
  * @return string
  */
 function pompey_chart( $lines, $height ) {
-
-	//$html =count( $lines );
 	$id = pompey_chart_id();
 	$html = "<div class=\"ct-chart\" id=\"$id\"></div>";
 	$script = pompey_chart_process( $id, $lines, $height );
@@ -182,13 +422,21 @@ function pompey_chart_process( $id, $lines, $height ) {
 			}
 		}
 	}
-	$data = pompey_chart_javascript( $id, $Date, $series1, $series2, $series3, $height );
+
+
+	$data = pompey_chart_javascript( null, $Date, $series1, $series2, $series3, $height );
 	return $data;
 }
 
 function pompey_chart_javascript( $id, $Date, $series1, $series2, $series3, $height = '500px') {
+	bw_trace2();
 
-	$Data = "var chart=new Chartist.Line( '#$id',{labels:[";
+	if ( null === $id ) {
+		$Data="var chart=new Chartist.Line( '.ct-chart',{labels:[";
+	} else {
+		$Data="var chart=new Chartist.Line( '#$id',{labels:[";
+		gob();
+	}
 	$Data .= $Date;
 	$Data .=  '],';
 	$Data .= 'series:[[';
@@ -201,16 +449,10 @@ function pompey_chart_javascript( $id, $Date, $series1, $series2, $series3, $hei
 	$Data .= "{fullWidth:true,width:'100%',height:'";
 	$Data .= $height;
 	$Data .= "',chartPadding:{right:0,left:0},";
-	$Data .= "plugins:[Chartist.plugins.tooltip(),Chartist.plugins.legend({legendNames:['Attendees','Joined','Members'],})]});";
+	$Data .= "plugins:[Chartist.plugins.tooltip(),Chartist.plugins.legend({legendNames:['Attendees','Joined','Members']})]});";
 	return $Data;
 }
-	/*
-	echo "<h1 style=\"text-align:center;\">Portsmouth MeetUp Statistics</h1>";
-	echo "<div class=\"ct-chart\"></div>";
-	echo "<script>";
-	echo $Data;
-	echo "</script>";
-	*/
+
 
 /*
  * $Data="var chart=new Chartist.Line('.ct-chart',{labels:[".$Date."],
